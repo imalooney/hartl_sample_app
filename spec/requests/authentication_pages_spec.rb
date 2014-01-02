759ced252354c1898a1f6ca72ba885c770b7880e
Elaine Looney <imalooney@gmail.com>
2014-01-02 17:49:02 -0600

    add test for error persistance bug

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,7 +13,7 @@ describe "AuthenticationPages" do
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
 
-      describe "after visiting another page"
+      describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
       end
