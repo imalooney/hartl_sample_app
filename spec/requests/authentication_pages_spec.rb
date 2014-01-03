f82cbdeaadd10d38584043111e7b9587ed9e16f5
Elaine Looney <imalooney@gmail.com>
2014-01-02 21:18:49 -0600

    update signout function

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,7 +4,7 @@ describe "AuthenticationPages" do
 
   subject { page }
 
-  describe "signin page" do
+  describe "signin" do
     before { visit signin_path }
 
     describe "with invalid information" do
@@ -23,7 +23,7 @@ describe "AuthenticationPages" do
       let(:user) { FactoryGirl.create(:user) }
       before do
         fill_in "Email",      with: user.email.upcase
-        fill_in "Password",   with: user.Password
+        fill_in "Password",   with: user.password
         click_button "Sign in"
       end
 
@@ -31,6 +31,11 @@ describe "AuthenticationPages" do
       it { should have_link('Profile',  href: user_path(user)) }
       it { should have_link('Sign out',  href: signout_path)}
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
